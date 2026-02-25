ba3a100b2336082cd570cdbb069da49e5ccc4d4e
djorjde1298 <marinkovicdjordje1@gmail.com>
2026-02-25 22:12:14 +0100

    fixed teacher_model

--- teacher_model.py
+++ teacher_model.py
@@ -10,29 +10,29 @@ from tensorflow.keras import regularizers
 def build_teacher_model(input_shape, num_classes = 10):
 
     model = Sequential()
-    model.add(Conv2D(32,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001),input_shape = input_shape))
+    model.add(Conv2D(32,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001),input_shape = input_shape))
     model.add(BatchNormalization())
-    model.add(Conv2D(32,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
+    model.add(Conv2D(32,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2,2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(64,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
+    model.add(Conv2D(64,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
     model.add(BatchNormalization())
-    model.add(Conv2D(64,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
+    model.add(Conv2D(64,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2,2)))
     model.add(Dropout(0.3))
 
-    model.add(Conv2D(128,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
+    model.add(Conv2D(128,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
     model.add(BatchNormalization())
-    model.add(Conv2D(128,(3,3),activation='elu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
+    model.add(Conv2D(128,(3,3),activation='relu',padding='same',kernel_regularizer=regularizers.l2(0.0001)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D((2,2)))
     model.add(Dropout(0.4))
 
     model.add(Flatten())
-    model.add(Dense(num_classes,activation='softmax'))
+    model.add(Dense(num_classes))
     model.summary()
 
     return model
