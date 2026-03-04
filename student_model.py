fa0c3bea394a79f6efcd313abf3b07ed1caf5e15
djorjde1298 <marinkovicdjordje1@gmail.com>
2026-03-04 23:20:40 +0100

    adjusted the student architecure and trained it again; better results

--- student_model.py
+++ student_model.py
@@ -6,18 +6,18 @@ from tensorflow.keras import regularizers
 def build_student_model(input_shape, num_classes = 10):
 
   model = Sequential()
-  model.add(Conv2D(16,(3,3), activation = 'relu', padding = 'same', kernel_regularizer = regularizers.l2(0.0001), input_shape = input_shape))
+  model.add(Conv2D(32,(3,3), activation = 'relu', padding = 'same', kernel_regularizer = regularizers.l2(0.0001), input_shape = input_shape))
   model.add(BatchNormalization())
   model.add(MaxPooling2D((2,2)))
   model.add(Dropout(0.2))
 
 
-  model.add(Conv2D(32, (3,3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.0001)))
+  model.add(Conv2D(64, (3,3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.0001)))
   model.add(BatchNormalization())
   model.add(MaxPooling2D((2,2)))
   model.add(Dropout(0.3))
   
-  model.add(Conv2D(64, (3,3), activation='relu', padding = 'same', kernel_regularizer=regularizers.l2(0.0001)))
+  model.add(Conv2D(128, (3,3), activation='relu', padding = 'same', kernel_regularizer=regularizers.l2(0.0001)))
   model.add(BatchNormalization())
   model.add(Flatten())
   
